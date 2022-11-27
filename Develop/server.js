7f6f9930d3da39465f3b40be44e706d5cc79ccac
Keshon Royster <k.royster512@gmail.com>
2022-11-26 19:34:17 -0600

    added post route

--- Develop/server.js
+++ Develop/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const uuid = require('uuid');
 
 const PORT = process.env.PORT || 3001;
 
@@ -16,13 +17,61 @@ app.use(express.static('public'));
 
 //creates notes route
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
     
 
 // Wildcard route to direct users to home page if no matching route has been found.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
+app.get('*', (req, res) => 
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    // res.status(200).json(`${req.method} request received for notes`);
+    console.info(`${req.method}`)
+});
+
+app.post('/api/notes', (req, res) => {
+  console.info(`${req.method} request received to add a review`)
+    
+    const {title, text} = req.body;
+
+    if (req.body) {
+      const newNotes = {
+        title,
+        text,
+        note_id: uuid
+      };
+
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.log(err);
+        }
+          else {
+            const notes = JSON.parse(data);
+
+            notes.push(newNotes);
+
+            fs.writeFile('./db/db.json',
+              JSON.stringify(notes),
+              (err) => 
+              err ? console.error(err)
+              : console.info('Updated Notes')
+            );
+          }
+      });
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+
+      console.log(response);
+      res.status(201).json(response);
+  } 
+  else {
+    res.status(500).json('Error in posting review');
+    }
 });
 
 app.listen(PORT, () =>
